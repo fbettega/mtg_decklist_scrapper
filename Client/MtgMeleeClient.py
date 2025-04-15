f08e1b49e8dd37ef7b806a8715900278f44607ca
fbettega <francois.bettega@gmail.com>
2025-04-15 17:53:46 +0200

    remove coockies refresh from main branch

--- Client/MtgMeleeClient.py
+++ Client/MtgMeleeClient.py
@@ -29,11 +29,10 @@ class MtgMeleeClient:
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0',
         'Content-Type': 'application/x-www-form-urlencoded'
                 })
-        if MtgMeleeClient._cookies_valid():
-            MtgMeleeClient._load_cookies(session)
-        else:
-            MtgMeleeClient._refresh_cookies(session)
-
+        # if MtgMeleeClient._cookies_valid():
+        #     MtgMeleeClient._load_cookies(session)
+        # else:
+        #     MtgMeleeClient._refresh_cookies(session)
         return session
     @staticmethod
     def _cookies_valid():
