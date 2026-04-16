1fe6521d52f5c9f95fdced660116793d622cf36e
fbettega <francois.bettega@gmail.com>
2026-04-16 19:18:51 +0200

    manual fix

--- fetch_tournament.py
+++ fetch_tournament.py
@@ -23,9 +23,8 @@ import Client.CardsrealmClient as CardsrealmClient
 
 # python fetch_tournament.py ./MTG_decklistcache/Tournaments 2025-01-01 2026-04-13 CardsRealm keepleague
 # python fetch_tournament.py ./MTG_decklistcache/Tournaments 2024-01-01 2025-03-30 mtgo keepleague
-# python fetch_tournament.py ./MTG_decklistcache/Tournaments 2025-03-01 2025-04-26 melee keepleague
 # python fetch_tournament.py ./MTG_decklistcache/Tournaments 2025-01-01 2025-02-28 melee keepleague
-# python fetch_tournament.py ./MTG_decklistcache/Tournaments 2025-04-01 2025-05-09 all keepleague
+# python fetch_tournament.py ./MTG_decklistcache/Tournaments 2026-01-01 2026-04-15 all keepleague
 # Configure logging to file and console
 def configure_logging(log_file_path):
     class Logger:
